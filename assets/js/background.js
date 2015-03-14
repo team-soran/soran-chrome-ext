a6d198b51a5b3a44945404c6299d8a034bc44b3d
Kang Hyojun <hyojun@admire.kr>
2015-03-14 21:14:46 +0900

    Message passing

---
+++ assets/js/background.js
@@ -0,0 +1,47 @@
+var naver = {
+  getName: function(next) {
+    next('abc');
+  }
+};
+var bugs = {
+}
+
+var routes = {
+  'naver': naver,
+  'bugs': bugs
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if(request['type'] == undefined) {
+    return sendResponse({'error': "request['type'] is undefined"});
+  }
+
+  var args = request['type'].split('.');
+  var service = args[0];
+  var func = args[1];
+
+  if(routes[service] == undefined || routes[service][func] == undefined) {
+    return sendResponse({'error': request['type'] + ' is undefined'});
+  }
+
+  routes[service][func](sendResponse, sender);
+});
+
+
+
+/*
+chrome.runtime.onConnect.addListener(function(port) {
+  console.log(port)
+  console.log(port.name)
+
+  port.onMessage.addListener(function(msg) {
+    if (msg.joke == "Knock knock")
+      port.postMessage({question: "Who's there?"});
+    else if (msg.answer == "Madame")
+      port.postMessage({question: "Madame who?"});
+    else if (msg.answer == "Madame... Bovary")
+      port.postMessage({question: "I don't get it."});
+  });
+});
+
+*/
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
 
+  var background = {
+    sendMessage: function(name, callback) {
+
+      chrome.runtime.sendMessage({'type': name}, function(response) {
+        callback(response)
+      });
+
+    }
+  }
+
   function onPageDetailsReceived() {
     console.log(arguments)
   }
 
   var getName = function(callback) {
-    chrome.runtime.onMessage.addListener(function() {
-      console.log('--- received');
-      console.log(arguments);
-    })
+    background.sendMessage('naver.getName', callback);
   }
+
 
   $('body').on('click', 'a', function(e){
     chrome.tabs.create({url: $(this).attr('href')});
